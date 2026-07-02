c9a95a1e43acdcefb504c43b7e9c4fab82cfee84
KJell09 <karl.angelo.mercado09@gmail.com>
2026-07-02 21:16:14 +0100

    Add RecentMeals/MealItem and update HomeScreen
    Introduce a reusable MealItem component and a RecentMeals list component to display example recent meals. Update the Home screen to import MacroGrid and render RecentMeals, and adjust the HomeHeader import path to the correct relative location. Styling relies on existing globalStyles and new local styles in MealItem.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
+import MacroGrid from '@/components/MacroGrid';
 import { Text, ScrollView } from "react-native";
 import { globalStyles  } from "@/styles/global";
-import HomeHeader from "@/components/HomeHeader"
+import HomeHeader from "../../components/HomeHeader"
+import RecentMeals from '@/components/RecentMeals';
 
 export default function HomeScreen() {
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>LeanLog</Text>
       <HomeHeader />
+      <MacroGrid />
+      <RecentMeals />
     </ScrollView>
   );
 }
